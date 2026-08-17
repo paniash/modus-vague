f80051c28ff498118284f26ecd9db89b7fa9de0c
paniash <public@ashishpanigrahi.com>
2026-08-18 02:01:11 +0300

    bump version to first minor version

--- modus-vague-theme.el
+++ modus-vague-theme.el
@@ -104,7 +104,7 @@
 (defcustom modus-vague-palette-overrides nil
   "Overrides for `modus-vague-palette'."
   :group 'modus-vague
-  :package-version '(modus-vague . "0.0.1")
+  :package-version '(modus-vague . "0.1.0")
   :type '(repeat (list symbol (choice symbol string)))
   :link '(info-link "(modus-themes) Palette overrides"))
 
