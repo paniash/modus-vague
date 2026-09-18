8819bf24e765a3654038690bfb02e67b86fa728f
paniash <public@ashishpanigrahi.com>
2026-09-19 00:13:15 +0300

    switch color peach for variable names with a normal weight

--- modus-vague-theme.el
+++ modus-vague-theme.el
@@ -144,8 +144,8 @@
   '(
     `(git-commit-summary ((,c :inherit bold :foreground ,iris)))
     `(tab-bar-tab-inactive ((,c :foreground ,gray)))
-    `(font-lock-type-face ((,c :inherit bold :foreground ,green))) ;; TODO: fix type-face not inheriting bold
-    `(font-lock-function-name-face ((,c :foreground ,peach))))
+    `(font-lock-type-face ((,c :weight normal :foreground ,green)))
+    `(font-lock-variable-name-face ((,c :foreground ,peach))))
   "Custom faces overriding the default faces of Modus themes.")
 
 ;;;###autoload
